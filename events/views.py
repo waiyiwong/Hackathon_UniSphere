176d15526a723eb95c30ca908a103f3ad35b661a
Crystal Johnson <mxcrystaljohnson@gmail.com>
2025-01-15 18:45:19 +0000

    Add CSRF Trusted Origin to settings, views added and templates.

--- events/views.py
+++ events/views.py
@@ -1,6 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Course, Event, Community
+from categories.models import Interests
 
 # Create your views here.
 def events_courses(request):
     return HttpResponse("Hello, Events and Courses!")
+def course_list(request):
+    interests = Interests.objects.all()
+    selected_interest = None
+    courses = Course.objects.all()
+
+    if request.method == 'GET' and 'interest' in request.GET:
+        selected_interest = get_object_or_404(Interests, pk=request.GET['interest'])
+        courses = courses.filter(interests=selected_interest)
+
+    context = {
+        'courses': courses,
+        'interests': interests,
+        'selected_interest': selected_interest,
+    }
+    return render(request, 'events/course_list.html', context)
+
+
+def event_list(request):
+    interests = Interests.objects.all()
+    selected_interest = None
+    events = Event.objects.all()
+
+    if request.method == 'GET' and 'interest' in request.GET:
+        selected_interest = get_object_or_404(Interests, pk=request.GET['interest'])
+        events = events.filter(interests=selected_interest)
+
+    context = {
+        'events': events,
+        'interests': interests,
+        'selected_interest': selected_interest,
+    }
+    return render(request, 'events/event_list.html', context)
+
+
+def community_list(request):
+    interests = Interests.objects.all()
+    selected_interest = None
+    communities = Community.objects.all()
+
+    if request.method == 'GET' and 'interest' in request.GET:
+        selected_interest = get_object_or_404(Interests, pk=request.GET['interest'])
+        communities = communities.filter(interests=selected_interest)
+
+    context = {
+        'communities': communities,
+        'interests': interests,
+        'selected_interest': selected_interest,
+    }
+    return render(request, 'events/community_list.html', context)
