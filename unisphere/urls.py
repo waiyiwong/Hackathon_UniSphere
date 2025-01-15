e7706bbe832dbbe7b75b24c7e94abe0b30dd9edd
Yazdan-Cristian (Dan) <danitaheri1@gmail.com>
2025-01-15 12:51:26 +0000

    categories been added

--- unisphere/urls.py
+++ unisphere/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from categories.views import categories
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('interests/', categories, name='interests'),
 ]
