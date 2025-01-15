6f190f6edeed5a877ab7b786afc71d8411fd928a
Wai Yi <wongwaverley@gmail.com>
2025-01-15 14:10:57 +0000

    delete about form and models.py

--- about/models.py
+++ about/models.py
@@ -1,19 +1,1 @@
 from django.db import models
-
-
-class About(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-
-    def __str__(self):
-        return self.title
-
-
-class FeedbackForm(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.EmailField()
-    message = models.TextField()
-    read = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"Feedback Form {self.name}"
