c437c9d082710e2549cca82318c8ab22884dd258
Crystal Johnson <mxcrystaljohnson@gmail.com>
2025-01-16 10:12:27 +0000

    Added compiled events, courses and communities and sort by interests.

--- unisphere/urls.py
+++ unisphere/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from categories.views import categories
-from events.views import events_courses, course_list
+from events.views import events_courses
 from account.views import profile_page
 
 urlpatterns = [
