3c4eb754d70a1dc111ffe5d8530797fa3b7546e5
Yazdan-Cristian (Dan) <danitaheri1@gmail.com>
2025-01-16 10:56:59 +0000

    interests

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,10 @@
+from django_summernote.admin import SummernoteModelAdmin
 from django.contrib import admin
+from .models import Interests
 
 # Register your models here.
+@admin.register(Interests)
+class Interests(SummernoteModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name', 'description')
+    list_filter = ('name', 'id')
