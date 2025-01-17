f427313103fe5955dc4a50adc38550cac917388b
Crystal Johnson <mxcrystaljohnson@gmail.com>
2025-01-17 10:59:07 +0000

    Add safe tags and add summernote functionality to profile edit form.

--- user_account/forms.py
+++ user_account/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 from .models import Profile
+from django_summernote.widgets import SummernoteWidget
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
+        widgets = {
+            'bio': SummernoteWidget(),
+        }
         fields = ['profile_picture', 'bio', 'interests', 'events', 'tickets', 
                   'courses', 'communities', 'ratings']
