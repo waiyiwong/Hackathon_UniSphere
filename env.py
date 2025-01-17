1a847e0d198fc8b6578f9dbc00447e6d9b6eaa89
Crystal Johnson <mxcrystaljohnson@gmail.com>
2025-01-17 18:55:23 +0000

    Add to env file ready to add to gitignore, styed course, events, communities and profile pages.

--- env.py
+++ env.py
@@ -2,3 +2,8 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:CHqKodstNS02@ep-calm-recipe-a2xoghc3.eu-central-1.aws.neon.tech/wink_crept_clone_195000")
+
+os.environ.setdefault("SECRET_KEY", "784fhq548of5u9-094-3900549034-0=320593")
+
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://351661715929979:JzT56iCiHO5k6muzl6RfSK7oEoc@ddu71pdb8")
